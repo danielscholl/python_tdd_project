f967021b26ea3c903e88ef83377374baa58710fa
danielscholl <dascholl@microsoft.com>
2025-01-03 21:53:13 -0600

    Initial Code

---
+++ README.md
@@ -0,0 +1,9 @@
+# Test Driven Development - Simple Transcript Analytics
+
+
+## Setup
+
+- Install dependencies: `uv sync`
+- Start the Test watcher `uv run ptw .`
+- Copy `.env.sample` to `.env` and set your OPENAI_API_KEY.
+- Run the script: `uv run main`
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,6 @@
 from src.tdd_project.main import main
-from io import StringIO
-import sys
-
-def test_true_is_true():
-     assert True is True
 
 def test_main_function(capsys):
     main()
     captured = capsys.readouterr()
-    assert captured.out == "Hello, Big World!\n"
+    assert captured.out == "Hello, Cruel World!\n"
